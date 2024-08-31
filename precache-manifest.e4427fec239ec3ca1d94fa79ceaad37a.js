ec03607da3db97bf52aa9e5aa6e9ecd1c9a96915
yash patle <105612209+yashpatle23@users.noreply.github.com>
2024-09-01 01:21:24 +0530

    Updates

---
+++ precache-manifest.e4427fec239ec3ca1d94fa79ceaad37a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8009aa91c3ce2e31bf04ccc97fff950",
+    "url": "/PORTFOLIO/index.html"
+  },
+  {
+    "revision": "36d42e7b8276c6ec48ba",
+    "url": "/PORTFOLIO/static/css/main.8db73e07.chunk.css"
+  },
+  {
+    "revision": "c0a515605348bee4326f",
+    "url": "/PORTFOLIO/static/js/2.99427b2a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/PORTFOLIO/static/js/2.99427b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d42e7b8276c6ec48ba",
+    "url": "/PORTFOLIO/static/js/main.d6c287cc.chunk.js"
+  },
+  {
+    "revision": "6a4940f82ccb863e8082",
+    "url": "/PORTFOLIO/static/js/runtime-main.0b1c833f.js"
+  },
+  {
+    "revision": "843c1fc065d2071896380b8f69d08668",
+    "url": "/PORTFOLIO/static/media/matt.843c1fc0.png"
+  }
+]);
